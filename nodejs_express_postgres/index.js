03500d4c3f5ef2edfe76a1c6aca17ad35f4ea806
Lukáš Cek <cek@seznam.cz>
2018-11-02 13:41:36 +0100

    Added package "pg" for PostreSQL connection.

--- nodejs_express_postgres/index.js
+++ nodejs_express_postgres/index.js
@@ -1,12 +1,39 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
 });
 
-app.post("/user/add", function(req, res) { 
-  res.send("OK");
+// Post example
+app.post('/user/add', function(req, res) { 
+  res.send('OK');
+});
+
+app.get('/getQuestion', function(req, res) { 
+  const { Pool } = require('pg');
+
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'test',
+    password: 'postgres',
+    port: 5432
+  });
+
+  pool.query('SELECT * FROM questions;', (sqlErr, sqlRes) => {
+    let response = '';
+    if (sqlErr) {
+      console.log(sqlErr);
+      res.send(sqlErr);
+    } else {
+      for (let row of sqlRes.rows) {
+        response += `${row.content}<br/>`
+      }
+    }
+    pool.end();
+    res.send(response);
+  });
 });
 
 app.get(/^(.+)$/, function(req, res){ 
@@ -16,5 +43,5 @@ app.get(/^(.+)$/, function(req, res){
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
